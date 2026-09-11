3a9247756e8c6b26741fc351f6a162d753015bf2
saiteja262k1919 <saiteja262k1919@gmail.com>
2026-09-11 12:17:16 +0530

    Update n_gram_token.py
    i added few lines with having gap

--- n_gram_token.py
+++ n_gram_token.py
@@ -1,17 +1,12 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
-
 nltk.download('punkt')
 nltk.download('punkt_tab')
-
 text = input("Enter a sentence: ")
 n = int(input("Enter the value of N: "))
-
 tokens = word_tokenize(text)
-
 result = list(ngrams(tokens, n))
-
 print(f"\n{n}-Grams:")
-for gram in result:
+for gram in result:   
     print(gram)
